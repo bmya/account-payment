76ce3862e3cfbb6f32bd2bcd8f656db30f247e64
Daniel Blanco <daniel@blancomartin.com.ar>
2018-12-06 12:56:00 -0300

    [WIP]

--- account_payment_group/static/src/js/hidebutton.js
+++ account_payment_group/static/src/js/hidebutton.js
@@ -4,10 +4,10 @@ odoo.define('account_payment_group.hidebutton.js', function(require) {"use_stric
         console.log( "hide button js loaded (2)" );
         $(".btn > span:contains('Guardar y Nuevo')").hide ();
         $(".btn > span:contains('Save & New')").hide ();
-        $(".modal-footer").find(".btn").click(function () {
+        $(".btn > span:contains('Guardar y Nuevo')").click(function () {
+            console.log( "click detected" );
             alert("No usar este boton porque no recalcula. Use guardar y cerrar en su lugar.");
-        return false;
-
+            return false;
         });
         /*$("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
         $("button > span:contains('Save & New')").parent().addClass("hide_button");*/
