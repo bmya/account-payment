c7c592a54b06649f74b6c4ec18959faa007c952b
Daniel Blanco <daniel@blancomartin.com.ar>
2018-12-06 12:45:03 -0300

    [WIP] change inside text

--- account_payment_group/static/src/js/hidebutton.js
+++ account_payment_group/static/src/js/hidebutton.js
@@ -2,7 +2,8 @@ odoo.define('account_payment_group.hidebutton.js', function(require) {"use_stric
     console.log( "hide button js loaded (1)" );
     $( document ).ready(function() {
         console.log( "hide button js loaded (2)" );
-        $("button > span:contains('Telephone Call')").hide ();
+        $("button > span:contains('Guardar y Nuevo')").hide ();
+        $("button > span:contains('Save & New')").hide ();
         /*$("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
         $("button > span:contains('Save & New')").parent().addClass("hide_button");*/
     });
