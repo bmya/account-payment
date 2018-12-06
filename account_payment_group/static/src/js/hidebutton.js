5f2edb5f6a4ad516c994f2196ccab1eb80bb234a
Daniel Blanco <daniel@blancomartin.com.ar>
2018-12-06 13:22:39 -0300

    [WIP]

--- account_payment_group/static/src/js/hidebutton.js
+++ account_payment_group/static/src/js/hidebutton.js
@@ -8,9 +8,10 @@ odoo.define('account_payment_group.hidebutton.js', function(require) {"use_stric
         /*$("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
         $("button > span:contains('Save & New')").parent().addClass("hide_button");*/
     });
-    $(".btn > span:contains('Guardar y Nuevo')").click(function () {
+    $('.modal .modal-footer button:eq(1)').click(function ()){
         console.log( "click detected" );
         alert("No usar este boton porque no recalcula. Use guardar y cerrar en su lugar.");
         return false;
-    });
+
+    };
 });
