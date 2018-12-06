6037933c31efbca731967a97df437eaa7888ac94
Daniel Blanco <daniel@blancomartin.com.ar>
2018-12-06 12:38:44 -0300

    [WIP[FIX] correct code

--- account_payment_group/static/src/js/hidebutton.js
+++ account_payment_group/static/src/js/hidebutton.js
@@ -1,6 +1,6 @@
 odoo.define('account_payment_group.hidebutton.js', function(require) {"use_strict";
     console.log( "hide button js loaded (1)" );
-    $("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");*/
+    $("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
     $( document ).ready(function() {
         console.log( "hide button js loaded (2)" );
         /*$("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
