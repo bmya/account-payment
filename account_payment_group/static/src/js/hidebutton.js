2c65b04a8d3915cff5b1fbdce1aed26785ac16e7
Daniel Blanco <daniel@blancomartin.com.ar>
2018-12-06 12:57:54 -0300

    [WIP] put click function outside doc ready

--- account_payment_group/static/src/js/hidebutton.js
+++ account_payment_group/static/src/js/hidebutton.js
@@ -4,12 +4,13 @@ odoo.define('account_payment_group.hidebutton.js', function(require) {"use_stric
         console.log( "hide button js loaded (2)" );
         $(".btn > span:contains('Guardar y Nuevo')").hide ();
         $(".btn > span:contains('Save & New')").hide ();
-        $(".btn > span:contains('Guardar y Nuevo')").click(function () {
-            console.log( "click detected" );
-            alert("No usar este boton porque no recalcula. Use guardar y cerrar en su lugar.");
-            return false;
-        });
+
         /*$("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
         $("button > span:contains('Save & New')").parent().addClass("hide_button");*/
+    });
+    $(".btn > span:contains('Guardar y Nuevo')").click(function () {
+        console.log( "click detected" );
+        alert("No usar este boton porque no recalcula. Use guardar y cerrar en su lugar.");
+        return false;
     });
 });
