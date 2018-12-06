ff6264ee4154a5fccb9ad42e3d7cb65e9c73877c
Daniel Blanco <daniel@blancomartin.com.ar>
2018-12-06 12:20:23 -0300

    [FIX] correct quotes

--- account_payment_group/static/src/js/hidebutton.js
+++ account_payment_group/static/src/js/hidebutton.js
@@ -1,3 +1,3 @@
 odoo.define('account_payment_group.hidebutton.js', function(require) {"use_strict";
-    $("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button”);
+    $("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
 })
