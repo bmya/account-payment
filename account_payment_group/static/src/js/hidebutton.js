5c161e988f4947f6d2410596a5f4551957228118
Daniel Blanco <daniel@blancomartin.com.ar>
2018-12-06 12:42:46 -0300

    [WIP] eliminate addClass

--- account_payment_group/static/src/js/hidebutton.js
+++ account_payment_group/static/src/js/hidebutton.js
@@ -1,8 +1,8 @@
 odoo.define('account_payment_group.hidebutton.js', function(require) {"use_strict";
     console.log( "hide button js loaded (1)" );
-    $("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
     $( document ).ready(function() {
         console.log( "hide button js loaded (2)" );
+        $("button > span:contains('Telephone Call')").hide ();
         /*$("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
         $("button > span:contains('Save & New')").parent().addClass("hide_button");*/
     });
