755302e1826e8061aab97a1090c06c85d8e373c9
Daniel Blanco <daniel@blancomartin.com.ar>
2018-12-06 12:33:54 -0300

    [WIP]

--- account_payment_group/static/src/js/hidebutton.js
+++ account_payment_group/static/src/js/hidebutton.js
@@ -1,8 +1,8 @@
-/* odoo.define('account_payment_group.hidebutton.js', function(require) {"use_strict";
-    $("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
-})*/
-$( document ).ready(function() {
-    console.log( "hide button js loaded" );
-    $("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
-    $("button > span:contains('Save & New')").parent().addClass("hide_button");
+odoo.define('account_payment_group.hidebutton.js', function(require) {"use_strict";
+    /* $("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");*/
+    $( document ).ready(function() {
+        console.log( "hide button js loaded" );
+        $("button > span:contains('Guardar y Nuevo')").parent().addClass("hide_button");
+        $("button > span:contains('Save & New')").parent().addClass("hide_button");
+    });
 });
